085fbd1d50247aa80eb6626e864254210a51dc9b
ilMitch <ilmitch.py@gmail.com>
2020-02-14 12:04:23 +0100

    added pdf reader

--- script.py
+++ script.py
@@ -12,6 +12,7 @@ import pandas as pd
 
 import matplotlib.pyplot as plt
 
+# Listing available PDFs
 root = "./"
 pattern = "*.pdf"
 files_lst = list()
@@ -22,3 +23,20 @@ for path, subdirs, files in os.walk(root):
             files_lst.append(os.path.join(path, name))
             
 print(f'\n{len(files_lst)} .csv files found!\n')
+
+# reading PDF text
+aPDF = files_lst[0]
+print(f"reading pdf: {aPDF}")
+
+f = open(aPDF,'rb')
+# List of every page's text.
+# The index will correspond to the page number.
+pdf_text = []
+
+pdf_reader = PyPDF2.PdfFileReader(f)
+
+for p in range(pdf_reader.numPages):
+    
+    page = pdf_reader.getPage(p)
+    
+    pdf_text.append(page.extractText())
